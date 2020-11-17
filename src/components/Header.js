3d4d46a78de8b04060221c4464e7907f18cb40d7
adams <adamsliu97@gmail.com>
2020-11-17 13:00:19 -0500

    hello

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+function Header() {
+  return (
+    <header id="header">
+      <div class="inner">
+        <a href="#" class="image avatar">
+          <img src="images/avatar.jpg" alt="" />
+        </a>
+        <h1>
+          <strong> I'm Adams Liu,</strong>
+          <br />a fullstack developer
+          <br />
+          who loves learning and is
+          <br /> passionate about creating <u>real</u> impact
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
